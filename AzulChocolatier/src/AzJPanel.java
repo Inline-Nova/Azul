2b2cc305982e4b96364ee8f8821bd4a440495737
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2022-11-07 10:14:20 -0600

    Images

--- AzulChocolatier/src/AzJPanel.java
+++ AzulChocolatier/src/AzJPanel.java
@@ -6,3 +6,23 @@ import java.io.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.util.*;
+
+public class AzJPanel extends JPanel{
+	private BufferedImage closeInstr, endScr, factory, instr, mainScr, 
+	noPat, pickPat, pickTile, tileBlu, tileblk, tileBrw, tileOne, tileRed, 
+	tileSc, tilewytl;
+	
+	
+	public AzJPanel(){
+		try {
+			closeInstr = ImageIO.read(AzJPanel.class.getResource("/Az_CloseInstructionsBtn.png"));
+		}catch(Exception E) {
+			System.out.println("Exception Error");
+			return;
+		}
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(closeInstr, 10,10,100,500,null);
+	}
+}
