3860135b14cbecf93ffc4c3cf9111d9ba97a1a92
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2022-11-07 11:11:51 -0600

    added pics, key/mouse listener

--- AzulChocolatier/src/AzJPanel.java
+++ AzulChocolatier/src/AzJPanel.java
@@ -4,26 +4,56 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.*;
 import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.util.*;
 
-public class AzJPanel extends JPanel{
+public class AzJPanel extends JPanel implements KeyListener, MouseListener{
 	private BufferedImage closeInstr, endScr, factory, instr, mainScr, 
 	noPat, pickPat, pickTile, tileBlu, tileblk, tileBrw, tileOne, tileRed, 
 	tileSc, tilewytl;
-	
+	private int choice;
 	
 	public AzJPanel(){
+		
 		try {
-			closeInstr = ImageIO.read(AzJPanel.class.getResource("/Az_CloseInstructionsBtn.png"));
+			closeInstr = ImageIO.read(AzJPanel.class.getResource("/AzulPics/Az_CloseInstructionsBtn.png"));
 		}catch(Exception E) {
 			System.out.println("Exception Error");
 			return;
 		}
+		
+		addKeyListener(this);
+		addMouseListener(this);
+	}
+	
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+		choice = e. getKeyChar()-'0';
+		System.out.println(choice);
+		repaint();
+	}
+	
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		System.out.println("loc is("+x+", "+y+")");
+		repaint();
+	}
+	
+	public void addNotify() {
+		super.addNotify();
+		requestFocus();
 	}
 	
 	public void paint(Graphics g){
 		g.fillRect(0, 0, getWidth(),  getHeight());
-		//g.drawImage(closeInstr, 10,10,100,500,null);
+		g.drawImage(closeInstr, 10,10,500,50,null);
 	}
 }
