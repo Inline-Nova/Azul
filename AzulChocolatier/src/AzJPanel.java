7cfa0f09fbab1220d6c4397f4b18d3a4d828184e
Jennifer Li <89474001+jennifer1i@users.noreply.github.com>
2022-11-07 07:34:08 -0600

    Update AzJPanel.java

--- AzulChocolatier/src/AzJPanel.java
+++ AzulChocolatier/src/AzJPanel.java
@@ -1,1 +1,8 @@
-
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.util.*;
