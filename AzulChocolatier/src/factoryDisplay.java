49f999c7e8aef355e85994d2a58f0ca39f329ce8
Inline-Nova <89323784+Inline-Nova@users.noreply.github.com>
2022-11-09 11:13:14 -0600

    changed factoryDisplay and playerBoard

--- AzulChocolatier/src/factoryDisplay.java
+++ AzulChocolatier/src/factoryDisplay.java
@@ -2,10 +2,31 @@ import java.util.*;
 
 public class factoryDisplay
 {
-  private ArrayList<Tile> factories;
+  private ArrayList<ArrayList<Tile>> factories; //hi
   
-  public factoryDisplay()
+  public factoryDisplay(Bag bag)
   {
+	  factories = new ArrayList<ArrayList<Tile>>();
+	  fillFactories(bag);
+  }
   
+  public void fillFactories(Bag bag)
+  {
+	for(int i = 0; i < 10; i++)
+	{
+		//factories.add(bag.getNewFacTiles()); //Bag class needs to be finished
+	}
+  }
+  
+  
+  public ArrayList<ArrayList<Tile>> getFactories()
+  {
+	  return factories;
+  }
+  
+  public ArrayList<Tile> getSectTiles(int sect)
+  {
+	  return factories.get(sect);
   }
 }
+
--- AzulChocolatier/src/playerBoard.java
+++ AzulChocolatier/src/playerBoard.java
@@ -1,32 +1,13 @@
 import java.util.*;
 
-public class factoryDisplay
+public class playerBoard
 {
-  private ArrayList<ArrayList<Tile>> factories; //hi
+ 
   
-  public factoryDisplay(Bag bag)
+  public playerBoard()
   {
-	  factories = new ArrayList<ArrayList<Tile>>();
-	  fillFactories(bag);
+	  
   }
-  
-  public void fillFactories(Bag bag)
-  {
-	for(int i = 0; i < 10; i++)
-	{
-		factories.add(bag.getNewFacTiles()); //Bag class needs to be finished
-	}
-  }
-  
-  
-  public ArrayList<ArrayList<Tile>> getFactories()
-  {
-	  return factories;
-  }
-  
-  public ArrayList<Tile> getSectTiles(int sect)
-  {
-	  return factories.get(sect);
-  }
+
 }
 
